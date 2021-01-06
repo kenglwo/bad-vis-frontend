2abd6888389241d66a7f64b78c181720268cd759
Kento Shigyo <kentoglwo@gmail.com>
2021-01-06 16:32:50 +0800

    :zap: add react-router and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "./components/common/Header";
+import Sidebar from "./components/common/Sidebar";
+import Top from "./components/top/Top";
+import Login from "./components/account/Login";
 import logo from "./assets/images/logo.svg";
 import "./assets/styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="p-0" fluid>
+        <BrowserRouter>
+          <Row>
+            <Header />
+          </Row>
+          <Row>
+            <Col md={2}>
+              <Sidebar />
+            </Col>
+            <Col>
+              <Switch>
+                <Route exact={true} path={"/"} component={Top} />
+                <Route path={"/login"} component={Login} />
+              </Switch>
+            </Col>
+          </Row>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
