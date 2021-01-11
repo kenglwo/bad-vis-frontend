071b22ee2f0241b819db40982d0d5cd031f3bc68
Kento Shigyo <kentoglwo@gmail.com>
2021-01-11 21:41:33 +0800

    :sparkles: integrete with Description and Checks

--- src/components/label_view/LabelView.tsx
+++ src/components/label_view/LabelView.tsx
@@ -5,13 +5,23 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import Form from "react-bootstrap/Form";
 import TopBar from "./TopBar";
+import Description from "./Description";
+import Checks from "./Checks";
 import LabelSetPanel from "./LabelSetPanel";
 // import "../../assets/styles/Header.scss";
 
 interface Props extends RouteComponentProps<{ userID: string }> {}
 interface State {
   imageSrc: string;
+  description: string;
+  checks: {
+    invalid: boolean;
+    notVis: boolean;
+    notBad: boolean;
+    cannotDecide: boolean;
+  };
 }
 
 class LabelView extends React.Component<Props, State> {
@@ -20,10 +30,22 @@ class LabelView extends React.Component<Props, State> {
 
     this.state = {
       imageSrc: "https://html5box.com/html5gallery/images/Swan_1024.jpg",
+      description: "",
+      checks: {
+        invalid: false,
+        notVis: false,
+        notBad: false,
+        cannotDecide: false,
+      },
     };
 
     this.onClickBackButton = this.onClickBackButton.bind(this);
     this.onClickForwardButton = this.onClickForwardButton.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeInvalid = this.onChangeInvalid.bind(this);
+    this.onChangeNotVis = this.onChangeNotVis.bind(this);
+    this.onChangeNotBad = this.onChangeNotBad.bind(this);
+    this.onChangeCannotDecide = this.onChangeCannotDecide.bind(this);
   }
 
   public onClickBackButton(e: React.MouseEvent<HTMLButtonElement>) {
@@ -36,9 +58,51 @@ class LabelView extends React.Component<Props, State> {
     console.log("click forward");
   }
 
+  public onChangeDescription(e: React.ChangeEvent<HTMLInputElement>) {
+    const value: string = e.target.value;
+    this.setState({ description: value });
+  }
+
+  public onChangeInvalid(e: React.ChangeEvent<HTMLInputElement>) {
+    const value: boolean = e.target.checked;
+    this.setState((prevState: State) => ({
+      checks: {
+        ...prevState.checks,
+        invalid: value,
+      },
+    }));
+  }
+  public onChangeNotVis(e: React.ChangeEvent<HTMLInputElement>) {
+    const value: boolean = e.target.checked;
+    this.setState((prevState: State) => ({
+      checks: {
+        ...prevState.checks,
+        notVis: value,
+      },
+    }));
+  }
+  public onChangeNotBad(e: React.ChangeEvent<HTMLInputElement>) {
+    const value: boolean = e.target.checked;
+    this.setState((prevState: State) => ({
+      checks: {
+        ...prevState.checks,
+        notBad: value,
+      },
+    }));
+  }
+  public onChangeCannotDecide(e: React.ChangeEvent<HTMLInputElement>) {
+    const value: boolean = e.target.checked;
+    this.setState((prevState: State) => ({
+      checks: {
+        ...prevState.checks,
+        cannotDecide: value,
+      },
+    }));
+  }
+
   public render() {
     return (
-      <Container fluid className="p-0">
+      <Container fluid className="">
         <Row>
           <TopBar
             onClickBackButton={this.onClickBackButton}
@@ -46,12 +110,29 @@ class LabelView extends React.Component<Props, State> {
           />
         </Row>
         <Row>
-          <Col md={8} className="p-0">
+          <Col md={6} className="">
             <Image src={this.state.imageSrc} fluid />
+            <div className="text-white">
+              <a href="#">Image Link</a>
+            </div>
           </Col>
-          <Col md={4} className="p-0">
-            <LabelSetPanel />
+          <Col md={3} className="text-white">
+            <Form>
+              <Description onChangeDescription={this.onChangeDescription} />
+              <Checks
+                onChangeInvalid={this.onChangeInvalid}
+                onChangeNotVis={this.onChangeNotVis}
+                onChangeNotBad={this.onChangeNotBad}
+                onChangeCannotDecide={this.onChangeCannotDecide}
+              />
+            </Form>
           </Col>
+          <Col md={3} className="text-white bg-secondary">
+            <div>Effects</div>
+          </Col>
+        </Row>
+        <Row className="mb-5">
+          <LabelSetPanel />
         </Row>
       </Container>
     );
