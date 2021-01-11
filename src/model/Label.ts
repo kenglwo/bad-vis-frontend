6f9b33d47d13e496385a472a52caf903caa6f651
Kento Shigyo <kentoglwo@gmail.com>
2021-01-11 21:42:18 +0800

    :sparkles: add stylesheet and model files for Label

---
+++ src/model/Label.ts
@@ -0,0 +1,37 @@
+export type LabelAPI = {
+  category: string;
+  name: string;
+  options: OptionsAPI[];
+  subcategories: SubcategoriesAPI[];
+  hierarchical: string;
+  grouped: string;
+  sorted: string;
+};
+
+export type OptionsAPI = {
+  tag: string;
+  subcategory?: string;
+  subcategories?: string[];
+  name: string;
+  description: string;
+  count: string;
+};
+
+export type SubcategoriesAPI = {
+  category: string;
+  group: string;
+  subcategory: string;
+  name: string;
+  description: string;
+};
+
+export type LabelCategory = {
+  categoryName: string;
+  options: LabelOption[];
+};
+
+export type LabelOption = {
+  optionName: string;
+  description: string;
+  subcategoryName: string;
+};
